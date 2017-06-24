4ec983f0a1ce028e05bb1a2c968497647d5fccea
Daisuke Inoue <hibariyashiki@gmail.com>
2017-06-24 19:44:10 +0900

    Yunoki-common

--- Yunoki.el
+++ Yunoki.el
@@ -4,13 +4,21 @@
 (defvar Yunoki-next-char-path (s-concat Yunoki-path "/next_char.rb"))
 (defvar Yunoki-modern-char-path (s-concat Yunoki-path "/modern_char.rb"))
 
-(defun Yunoki-next-char nil
+(defun Yunoki-common (path)
     (interactive)
     (save-excursion
         (backward-char)
         (set-mark-command nil)
         (forward-char)
-        (shell-command-on-region (region-beginning) (region-end) (s-concat "ruby " Yunoki-next-char-path) nil t))
+        (shell-command-on-region (region-beginning) (region-end) (s-concat "ruby " path) nil t))
     (forward-char))
+
+(defun Yunoki-next-char nil
+    (interactive)
+    (Yunoki-common Yunoki-next-char-path))
+
+(defun Yunoki-modern-char nil
+    (interactive)
+    (Yunoki-common Yunoki-modern-char-path))
 
 (provide 'Yunoki)
